5b6c19ea11510f9cf022704ddd1a4d6d7937ede5
DrFaelSan <rafaelvplima@gmail.com>
2019-08-17 17:22:27 -0300

    First Commit

--- ApiProject/Controllers/EleitoresController.cs
+++ ApiProject/Controllers/EleitoresController.cs
@@ -17,23 +17,47 @@ namespace ApiProject.Controllers
         {
             var cadastro = new EleitorCore(eleitor).CadastroEleitor();
             if (cadastro.Status)
-                return Created("https://localhost", cadastro.Resultado);
+                return Created($"https://localhost:44323/api/Eleitores/{eleitor.Id}", cadastro.Resultado);
 
             return BadRequest(cadastro.Resultado);
         }
 
 
         [HttpGet("{id}")]
-        public async Task<IActionResult> Get(string id) => Ok(new Eleitor());
+        public async Task<IActionResult> GetById(string id){
+            var eleitor = new EleitorCore().ProcurarPorID(id);
+            if (eleitor.Status)
+                return Ok(eleitor.Resultado);
+
+            return BadRequest(eleitor.Resultado);
+        }
 
         [HttpGet]
-        public async Task<IActionResult> Get() => Ok(new Eleitor());
+        public async Task<IActionResult> GetAll()        {
+            var todos = new EleitorCore().ProcurarTodos();
+            if (todos.Status)
+                return Ok(todos.Resultado);
 
+            return BadRequest(todos.Resultado);
+        }
         [HttpPut("{id}")]
-        public async Task<IActionResult> Put(string id) => NoContent();
+        public async Task<IActionResult> Put(string id ,[FromBody]Eleitor eleitor)
+        {
+            var atualizar = new EleitorCore().AtualizarPorID(id,eleitor);
+            if (atualizar.Status)
+                return Ok(atualizar.Resultado);
+
+            return BadRequest(atualizar.Resultado);
+        }
 
         [HttpDelete("{id}")]
-        public async Task<IActionResult> Delete(string id) => Accepted();
+        public async Task<IActionResult> Delete(string id)
+        {
+            var excluir = new EleitorCore().DeletarPorID(id);
+            if (excluir.Status)
+                return Ok(excluir.Resultado);
 
+            return BadRequest(excluir.Resultado);
+        }
     }
 }
--- Core/EleitorCore.cs
+++ Core/EleitorCore.cs
@@ -5,6 +5,7 @@ using System.Text;
 using FluentValidation;
 using Microsoft.AspNetCore.Mvc;
 using Core.util;
+using System.Linq;
 
 namespace Core
 {
@@ -25,6 +26,7 @@ namespace Core
                 .NotNull()
                 .WithMessage("O nome deve ser preenchido e deve ter o mínimo de 3 caracteres");
         }
+        public EleitorCore() { }
 
         public Retorno CadastroEleitor() {
 
@@ -50,5 +52,92 @@ namespace Core
 
             }
 
+        public Retorno ProcurarPorID(string id)
+        {
+            var db = file.ManipulacaoDeArquivos(true, null);
+
+            if (db.sistema == null)
+                db.sistema = new Sistema();
+
+            if (db.sistema.Eleitores.Any(e => e.Id.ToString().Equals(id)))
+            {
+                return new Retorno() { Status = true, Resultado = db.sistema.Eleitores.SingleOrDefault(e => e.Id.ToString().Equals(id)) };
+            }
+            else
+                return new Retorno() { Status = false, Resultado = "Não existe um eleitor com esse ID." };
+
+        }
+
+        public Retorno ProcurarTodos()
+        {
+            var db = file.ManipulacaoDeArquivos(true, null);
+
+            if (db.sistema == null)
+                db.sistema = new Sistema();
+
+            if (db.sistema.Eleitores.Any())
+            {
+                return new Retorno() { Status = true, Resultado = db.sistema.Eleitores };
+            }
+            else
+                return new Retorno() { Status = false, Resultado = "Não existe nenhum elemento." };
+
+        }
+
+        public Retorno DeletarPorID(string id)
+        {
+            var db = file.ManipulacaoDeArquivos(true, null);
+
+            if (db.sistema == null)
+                db.sistema = new Sistema();
+
+            if (db.sistema.Eleitores.Exists(e => e.Id.ToString().Equals(id)))
+            { 
+                db.sistema.Eleitores.Remove(db.sistema.Eleitores.Single(e => e.Id.ToString().Equals(id)));
+                file.ManipulacaoDeArquivos(false, db.sistema);
+
+                return new Retorno() { Status = true, Resultado = "Eleitor Deletado com sucesso." };
+            }
+            else
+                return new Retorno() { Status = false, Resultado = "Não existe um eleitor com esse ID ,nada foi deletado." };
+
+        }
+
+        public Retorno AtualizarPorID(string id,Eleitor eleitor)
+        {
+       
+            // Caso o modelo seja válido, escreve no arquivo db
+            var db = file.ManipulacaoDeArquivos(true, null);
+
+            if (db.sistema == null)
+                db.sistema = new Sistema();
+
+            if (db.sistema.Eleitores.Exists(e => e.Id.ToString().Equals(id)))
+            {
+                db.sistema.Eleitores.Add(TrocarDadosDeEleitores(eleitor, db.sistema.Eleitores.SingleOrDefault(e => e.Id.ToString().Equals(id))));
+                file.ManipulacaoDeArquivos(false, db.sistema);
+                return new Retorno() { Status = true, Resultado = _eleitor };
+            }
+            else
+                return new Retorno() { Status = false, Resultado = "Já existe um eleitor com esse documento." };
+
+        }
+
+        public Eleitor TrocarDadosDeEleitores(Eleitor FDP1, Eleitor Objeto)
+        { 
+            if (FDP1.Nome == null)
+                FDP1.Nome = Objeto.Nome;
+            if (FDP1.Idade == 0)
+                FDP1.Idade = Objeto.Idade;
+            if (FDP1.Sexo == null)
+                FDP1.Sexo = Objeto.Sexo;
+            if (FDP1.Documento == null)
+                FDP1.Documento = Objeto.Documento;
+       
+            return FDP1;
+        }
+
     }
+    
 }
+
