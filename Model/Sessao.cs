9fd7c60fe1e134d0a8195e338724a285ffb919e9
matheus <matheus-397@bol.com.br>
2019-08-26 16:58:10 -0300

    Detalhamento modelagem

--- Model/Sessao.cs
+++ Model/Sessao.cs
@@ -5,8 +5,8 @@ namespace Model
     public class Sessao : Base
     {
         public bool Aberta;
-        public List<Urna> Urnas { get; set; } = new List<Urna>();
-        public List<Eleitor> eleitoresUrna { get; set; } = new List<Eleitor>();
-        public List<Pauta> pautasUrna { get; set; } = new List<Pauta>();
+        public List<Urna> urnasSessao { get; set; } = new List<Urna>();
+        public List<Eleitor> eleitoresSessao { get; set; } = new List<Eleitor>();
+        public List<Pauta> pautasSessao { get; set; } = new List<Pauta>();
     }
 }
